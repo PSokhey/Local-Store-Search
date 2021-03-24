cb0756bd3fe2b8f614fe1eea5e49b1683a8ff430
madhav-ramdev <mramdev1@my.bcit.ca>
2021-03-24 22:45:02 +0530

    different file for api js

---
+++ scripts/firebase-api-storesearch.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAzSTPSshHMenAtU6yzhhMVv6aXRxr7u7A",
+    authDomain: "localstoresearchproject.firebaseapp.com",
+    projectId: "localstoresearchproject",
+    storageBucket: "localstoresearchproject.appspot.com",
+    messagingSenderId: "1060426772473",
+    appId: "1:1060426772473:web:264cd542e1bb54a3ea9728"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
