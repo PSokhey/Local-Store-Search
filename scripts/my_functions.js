0628a790a4b62112bab9ed85cf49b9762c332ef8
Joseph Dobrzanski <jdobrzanski2@my.bcit.ca>
2021-04-06 16:12:13 -0700

    added function to populate database (things structured correctly). Added functionality to read store details from url and paste that store's information in the related fields on the webpage

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -1,7 +1,5 @@
 // helper function for posting 1 review. Receives review collection, posts specified information and updates number of comments
 function displayOneReview(doc) {
-
-
     var userUID = doc.data().username;
     var usernameR = "";
     var ratingR = doc.data().rating * 2;
@@ -47,8 +45,6 @@ function displayOneReview(doc) {
             var numberComment = +document.getElementById("numComment").innerHTML;
             $("#numComment").text(numberComment + 1);
         })
-
-
 }
 
 // function that runs at on page start to load all comments for this store 
@@ -57,6 +53,7 @@ function displayReviews() {
     $("#numComment").text(0);
     $("#comments-go-here").text("");
 
+    // post reviews, one at a time
     db.collection("test_data_joseph") // *** using test folder for now
         .orderBy("timestamp", "desc").limit(5)
         .get()
@@ -64,9 +61,13 @@ function displayReviews() {
             snap.forEach(function (doc) {
                 displayOneReview(doc)
             })
+            // update the store's overall rating
+            //updateStoreRating();
         })
 }
 displayReviews();
+
+
 
 // function for handling when a new review is posted
 function createComment() {
@@ -127,124 +128,87 @@ function updateRating(inputRating) {
     document.getElementById("reviewRatingValue").innerHTML = rating;
 }
 
-
+// function to post the store rating 
+function updateStoreRating() {
+    var halfStar = 0;
+    var rating = "";
+    for (var i = 0; i < 5; i++) {
+        if (i < Math.floor(inputRating / 2)) {
+            //console.log("star");
+            rating = rating + "&#xf005;";
+        } else if ((inputRating % 2 == 1) && !halfStar) {
+            //console.log("halfstar");
+            halfStar = 1;
+            rating = rating + "&#xf123;";
+        } else {
+            //console.log("empty");
+            rating = rating + "&#xf006;";
+        }
+    }
+    document.getElementById("storeRating").innerHTML = rating;
+}
 
 
 function writeData() {
     //this is an array of JSON objects copied from open source data
     var stores = [{
-            "datasetid": "web-cam-url-links",
-            "recordid": "01d6f80e6ee6e7f801d2b88ad7517bd05223e790",
-            "fields": {
-                "url": "http://images.drivebc.ca/bchighwaycam/pub/html/www/17.html",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.136736007805,
-                        49.2972589838826
-                    ]
-                },
-                "mapid": "TCM015",
-                "name": "Stanley Park Causeway"
+            "storeInformation": {
+                "name": "Digikey",
+                "rating": "4.5",
+                "storeBio": "This is an online store that sells over 100,000 different electronic componenets to customers around the world! One-deay shipping if orders are placed before 15:00 EST for locations in North America.",
+                "address": "889 East 57th Avenue",
+                "postalCode": "V1R 7G8",
+                "hours": [
+                    "8:00AM - 15:00PM",
+                    "8:00AM - 15:00PM",
+                    "8:00AM - 15:00PM",
+                    "8:00AM - 15:00PM",
+                    "8:00AM - 15:00PM",
+                    "9:00AM - 17:00PM",
+                    "9:00AM - 17:00PM"
+                ]
             },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
+            "externalLinks": {
+                "link1": "www.digikey.com",
+                "phoneNumber": "1-800-344-4539",
+                "email": "sales@digikey.com"
+            },
+            "imageGallery": {
+                "image1": "./images/01Grocery5.jpg"
+            },
+            "customerReviews": {}
         },
         {
-            "datasetid": "web-cam-url-links",
-            "recordid": "d95ead494c2afbb5f47efdc26bf3ea8c6b8b2e22",
-            "fields": {
-                "url": "http://images.drivebc.ca/bchighwaycam/pub/html/www/20.html",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.129968,
-                        49.324891
-                    ]
-                },
-                "mapid": "TCM017",
-                "name": "North End 2"
+            "storeInformation": {
+                "name": "Newark",
+                "rating": "4.8",
+                "storeBio": "Choose from a vast selection of industrial, commercial, and consumer grade electronic products. No shipping charges will be applied for orders over $150CAD! Worldwide shipping is available.",
+                "address": "1600 Pennsylvania Avenue NW",
+                "postalCode": "37188",
+                "hours": [
+                    "8:00AM - 20:00PM",
+                    "8:00AM - 20:00PM",
+                    "8:00AM - 17:30PM",
+                    "8:00AM - 20:00PM",
+                    "8:00AM - 20:00PM",
+                    "10:00AM - 21:00PM",
+                    "11:30AM - 21:00PM"
+                ]
             },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "8651b55b799cac55f9b74d654a88f3500b6acd64",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie49.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.116492357278,
-                        49.2261139995231
-                    ]
-                },
-                "mapid": "TCM024",
-                "name": "Cambie St and W 49th Av",
-                "geo_local_area": "Oakridge"
+            "externalLinks": {
+                "link1": "www.canada.newark.com",
+                "phoneNumber": "1-800-639-2758",
+                "email": "order@newark.com"
             },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "f66fa2c58d19e3f28cf8b842bfa1db073e32e71b",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie41.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.116192190431,
-                        49.2335434721856
-                    ]
-                },
-                "mapid": "TCM025",
-                "name": "Cambie St and W 41st Av",
-                "geo_local_area": "South Cambie"
+            "imageGallery": {
+                "image1": "./images/furniture.jpg"
             },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "7c3afe1d3fe4c80f24260a4946abea3fb15b7017",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/cambie25.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.115406053889,
-                        49.248990875309
-                    ]
-                },
-                "mapid": "TCM026",
-                "name": "Cambie St and W King Edward Av",
-                "geo_local_area": "South Cambie"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
-        },
-        {
-            "datasetid": "web-cam-url-links",
-            "recordid": "7fea7df524a205c0c0eb8efcc273345356cbe8d1",
-            "fields": {
-                "url": "https://trafficcams.vancouver.ca/mainTerminal.htm",
-                "geom": {
-                    "type": "Point",
-                    "coordinates": [
-                        -123.100028035364,
-                        49.2727762979223
-                    ]
-                },
-                "mapid": "TCM028",
-                "name": "Main St and Terminal Av",
-                "geo_local_area": "Downtown"
-            },
-            "record_timestamp": "2021-03-22T10:32:40.391000+00:00"
+            "customerReviews": {}
         }
     ];
 
     stores.forEach(function (oneStore) { //cycle thru json objects in array
         console.log(oneStore); //just to check it out
-        db.collection("webcams").add(oneStore) //add this new document
-            .then(function (oneStore) { //success 
-                console.log("wrote to webcams collection " + doc.id);
-            })
+        db.collection("storesDatabase").add(oneStore)
     })
 }
