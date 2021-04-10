440996867e6b447553e6a01eed8fb605d760dc23
Joseph Dobrzanski <jdobrzanski2@my.bcit.ca>
2021-04-09 18:57:52 -0700

    completed search page to display search results as cards. Each card changes background color/cursor when hovered over and links to the storeInformation.html page with the appropriate info in the url. Added back button to storeInformation.html. Other minor code changes

--- scripts/search_scripts.js
+++ scripts/search_scripts.js
@@ -6,10 +6,21 @@ function getSearchBar() {
         window.location.href = "showSearch.html?search=" + store;
     })
 }
-getSearchBar();
 
+// put text back into search bar
+function refillSearchBar() {
+    console.log("am here");
+    const parsedUrl = new URL(window.location.href);
+    // extract id from url, assign to variable
+    var search = (parsedUrl.searchParams.get("search"));
+    //console.log(parsedUrl.searchParams.get("storeID"));
 
-// look search words in url and find relevant results
+    // put search terms back into search bar for user to have for reference (for future searches)
+    document.getElementById("storeSearch").value = search;
+}
+
+// function for taking search terms from url, and seeing how relevant they are to DB stores.
+// passes list (in descending relevance) to another function to display on page
 function getResults() {
     // grab url
     const parsedUrl = new URL(window.location.href);
@@ -89,21 +100,21 @@ function getResults() {
 
             displayResults(storeArray)
 
-            ///* after sort logging
+            /* after sort logging
             console.log("storeArray: " + storeArray);
             console.log("relevance: " + relevance);
             console.log("r index: " + relevanceIndex);
-            //*/
+            */
         })
 }
 
-// function that runs at on page start to display results of search 
+// function that handles displaying information on page (what search terms are used and cards for relevant results)
 function displayResults(storeList) {
     // grab url
     const parsedUrl = new URL(window.location.href);
     // extract id from url, assign to variable
     var search = (parsedUrl.searchParams.get("search"));
-    console.log(parsedUrl.searchParams.get("storeID"));
+    //console.log(parsedUrl.searchParams.get("storeID"));
 
     // post search terms on screen. Handle case where nothing was searched (i.e. page visited manually/url tampered with)
     if (search == null) {
@@ -113,78 +124,85 @@ function displayResults(storeList) {
         $("#searchTerms").text(search);
     }
 
-    // put search terms back into search bar for user to have for reference (for future searches)
-    document.getElementById("storeSearch").value = search;
+    //console.log(search);
 
-    // display results on screen
+    // post relevant stores from search on screen. Handle case where no relevant results were found
+    if ((storeList == null) || (storeList.length == 0)) {
+        $("#searchTermsFull").text("No relevant stores were found for this search!");
+        $("#errorMessage").text("Please check that there are no spelling errors, and consider broadening the search terms used.");
+    } else {
+        // display results on screen
+        for (var j = 0; j < storeList.length; j++) {
+            db.collection("storesDatabase")
+                .doc(storeList[j]) // get specific store document
+                .get() // read
+                .then(function (doc) { // display details!
+                    //displayOneReview(doc, storeList) // send to another function to write to page
+                    console.log(doc.id);
 
-    console.log(search);
+                    var nameS = doc.data().storeInformation.name;
+                    var ratingS = doc.data().cumulativeRating * 2;
+                    var imageS = doc.data().imageGallery.image1;
+                    var addressS = doc.data().storeInformation.address;
+                    var phoneS = doc.data().externalLinks.phoneNumber;
 
-    /*
-        db.collection("storesDatabase")
-            .doc(id)
-            .collection("customerReviews") // store ID that we extracted
-            .get() // REAC async
-            .then(function (snap) { // display details!
-                snap.forEach(function (doc) {
-                    displayOneReview(doc)
+                    // handle potential error case
+                    if ((ratingS == null) || (ratingS == NaN)) {
+                        ratingsS = 0;
+                    }
+
+                    var ratingString = "";
+                    var halfStar = 0;
+                    for (var i = 0; i < 5; i++) {
+                        if (i < Math.floor(ratingS / 2)) {
+                            ratingString = ratingString + "&#xf005;";
+                        } else if ((ratingS % 2 == 1) && !halfStar) {
+                            halfStar = 1;
+                            ratingString = ratingString + "&#xf123;";
+                        } else {
+                            ratingString = ratingString + "&#xf006;";
+                        }
+                    }
+                    var codeString = '<div class="card mb-3" id="' + doc.id + '">' +
+                        '<div class="row g-0">' +
+                        '<div class="col-md-3" style="padding: 10px 10px 0 10px; text-align: center; max-width: 115px;">' +
+                        '<img src="' + imageS + '" alt="userIcon" style="width: 100px; height: 100px; display: block;">' +
+                        '<p style="display: inline-block" class="star1user fa">' + ratingString + '</p>' +
+                        '</div>' +
+                        '<div class="col-md-9">' +
+                        '<div class="card-body">' +
+                        '<h5 class="card-title username">' + nameS + '</h5>' +
+                        '<p class="card-text comment">' + phoneS + '</p>' +
+                        '<p class="card-text"><small class="text-muted datePosted">' + addressS + '</small></p>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
+                    $("#resultsBody").append(codeString);
+                    addStoreListener(doc.id);
+                    //console.log(j);
                 })
-                // update the store's overall rating
-                updateStoreRating();
-            })
-            */
+        }
+    }
 }
 
-// helper function for posting 1 review. Receives review collection, posts specified information and updates number of comments
-function displayOneResult(doc) {
-    var nameS = doc.data().storeInformation.name;
-    var ratingS = doc.data().cumulativeRating * 2;
-    var imageS = doc.data().imageGallery.image1; // *** need to change to stored picture
-    var addressS = doc.data().storeInformation.address;
-    var phoneS = doc.data().externalLinks.phoneNumber;
+// function for adding listener to results posting on page
+function addStoreListener(id) {
+    document.getElementById(id)
+        .addEventListener("click", function () {
+            const parsedUrl = new URL(window.location.href);
+            var searchID = (parsedUrl.searchParams.get("search"));
+            window.location.href="storeInformation.html?search=" + searchID + "&" + "storeID=" + id;
+        });
 
-    // handle potential error case
-    if ((ratingS == null) || (ratingS == NaN)) {
-        ratingsS = 0;
-    }
+    var div = document.getElementById(id);
 
+    $("#" + id).css("cursor", "pointer");
+    $("#" + id).css("background-color", "#f2f2f2");
+    $("#" + id).hover(function(){
+        $(this).css("background-color", "#d9d9d9");
+        }, function(){
+        $(this).css("background-color", "#f2f2f2");
+      });
 
-    // convert UID into username and profile picture
-    db.collection("users").doc(userUID)
-        .get()
-        .then(function (snap) {
-            usernameR = snap.data().name;
-            // GET IMAGE URL HERE
-            var ratingString = "";
-            var halfStar = 0;
-            for (var i = 0; i < 5; i++) {
-                if (i < Math.floor(ratingR / 2)) {
-                    ratingString = ratingString + "&#xf005;";
-                } else if ((ratingR % 2 == 1) && !halfStar) {
-                    halfStar = 1;
-                    ratingString = ratingString + "&#xf123;";
-                } else {
-                    ratingString = ratingString + "&#xf006;";
-                }
-            }
-            var codeString = '<div class="card mb-3">' +
-                '<div class="row g-0">' +
-                '<div class="col-md-3" style="padding: 10px 10px 0 10px; text-align: center; max-width: 115px;">' +
-                '<img src="' + imageR + '" alt="userIcon" style="width: 100px; height: 100px; display: block;">' +
-                '<p style="display: inline-block" class="star1user fa">' + ratingString + '</p>' +
-                '</div>' +
-                '<div class="col-md-9">' +
-                '<div class="card-body">' +
-                '<h5 class="card-title username">' + usernameR + '</h5>' +
-                '<p class="card-text comment">' + commentR + '</p>' +
-                '<p class="card-text"><small class="text-muted datePosted">' + dateR + '</small></p>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>';
-            $("#comments-go-here").append(codeString);
-
-            var numberComment = +document.getElementById("numComment").innerHTML;
-            $("#numComment").text(numberComment + 1);
-        })
 }
